0da652f3486c124e2bdbd1b49d08e37af5c54934
FreeR <freer@mail.ru>
2016-03-13 21:29:18 +0300

    create platron payment module for NetCat 5.3

---
+++ netcat/make.sql
@@ -0,0 +1,1 @@
+INSERT INTO `netcat`.`Classificator_PaymentSystem` (`PaymentSystem_ID`, `PaymentSystem_Name`, `PaymentSystem_Priority`, `Value`, `Checked`) VALUES (NULL, 'Оплата Platron онлайн', '1', 'nc_payment_system_platron', '1');
